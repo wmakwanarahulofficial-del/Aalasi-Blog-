32f76d68ae5eef0baa9205a95b967d2cf5782723
wmakwanarahulofficial-del <w.makwanarahulofficial@gmail.com>
2026-05-24 01:14:52 +0530

    Set up React app with routing and theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,21 @@
 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider } from './context/ThemeContext';
+import './i18n/config';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </ThemeProvider>
+  </StrictMode>,
+);
