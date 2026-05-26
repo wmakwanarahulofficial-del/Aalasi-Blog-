7850a9d2c74c60c0a951461cf56dee5abb463a8c
wmakwanarahulofficial-del <w.makwanarahulofficial@gmail.com>
2026-05-26 17:33:28 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import React from "react";
+hereimport React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>AALASI BLOG WORKING ✅</h1>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
