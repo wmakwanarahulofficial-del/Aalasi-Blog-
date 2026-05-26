6b69c5d3f8ed9744aa917806393aca5844250c2a
wmakwanarahulofficial-del <w.makwanarahulofficial@gmail.com>
2026-05-26 17:20:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,9 @@
-import { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { MainLayout } from './layouts/MainLayout';
-import { HomePage } from './pages/HomePage';
-import { BlogDetails } from './pages/BlogDetails';
-import { Dashboard } from './pages/Dashboard';
-import { BlogListPage } from './pages/BlogListPage';
-import { CategoriesPage } from './pages/CategoriesPage';
-import { CategoryPage } from './pages/CategoryPage';
-import { SearchPage } from './pages/SearchPage';
-import { AuthPage } from './pages/AuthPage';
-import { AboutPage, ContactPage } from './pages/StaticPages';
-import { ProtectedRoute } from './components/ProtectedRoute';
+hereimport React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-export default function App() {
-  useEffect(() => {
-    fetch('/api/track-view', { method: 'POST' });
-  }, []);
-
-  return (
-    <Routes>
-      {/* Public area with MainLayout */}
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="latest" element={<BlogListPage type="latest" />} />
-        <Route path="trending" element={<BlogListPage type="trending" />} />
-        <Route path="categories" element={<CategoriesPage />} />
-        <Route path="category/:slug" element={<CategoryPage />} />
-        <Route path="search" element={<SearchPage />} />
-        
-        <Route path="about" element={<AboutPage />} />
-        <Route path="contact" element={<ContactPage />} />
-        
-        <Route path="blog/:slug" element={<BlogDetails />} />
-      </Route>
-
-      {/* Admin Panel Hidden Route */}
-      <Route path="/aalsi-admin-login" element={<AuthPage />} />
-
-      {/* Protected Admin Routes */}
-      <Route path="/admin" element={<ProtectedRoute />}>
-        <Route index element={<Navigate to="dashboard" replace />} />
-        <Route path=":tab" element={<Dashboard />} />
-      </Route>
-    </Routes>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
